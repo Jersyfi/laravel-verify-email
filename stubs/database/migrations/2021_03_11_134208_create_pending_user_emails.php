ab000c05e8b784fb57e9abba5462d90cd889146b
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2021-03-11 18:39:02 +0100

    Wrong Class Name

--- stubs/database/migrations/2021_03_11_134208_create_pending_user_emails.php
+++ stubs/database/migrations/2021_03_11_134208_create_pending_user_emails.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class PendingUserEmails extends Migration
+class CreatePendingUserEmails extends Migration
 {
     /**
      * Run the migrations.
